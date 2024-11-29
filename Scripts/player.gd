2687142318422cd689cecbd14def315d211938c0
Jalenos <Jalen.huang55@gmail.com>
2024-11-29 12:59:03 -0800

    pilot

---
+++ Scripts/player.gd
@@ -0,0 +1,23 @@
+#extends CharacterBody2D
+#
+#@export var speed = 400
+#
+#func get_input():
+	#var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#velocity = input_direction * speed
+#
+#func _physics_process(_delta):
+	#get_input()
+	#move_and_slide()
+#
+#@onready var _animated_sprite = $AnimationPlayer
+#
+#func _process(_delta):
+	#if Input.is_action_pressed("ui_right"):
+		#_animated_sprite.play("runRight")
+	#else:
+		#_animated_sprite.play("idleRight")
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
