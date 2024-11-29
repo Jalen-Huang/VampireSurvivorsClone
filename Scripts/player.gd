021f1511d1f0ea4a8c53f83ca4747acdd22ccfb5
Jalenos <Jalen.huang55@gmail.com>
2024-11-29 14:19:06 -0800

    Fireball testing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 100
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -12,12 +12,19 @@ func _physics_process(_delta):
 
 @onready var _animated_sprite = $PlayerAnimation
 
-func _process(_delta):
-	if Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("walkUp")
-	else:
-		_animated_sprite.play("idleUp")
+func s_process(_delta):
+	pass
+	#if Input.is_action_pressed("ui_up"):
+		#_animated_sprite.play("walkUp")
+	#else:
+		#_animated_sprite.play("idleUp")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func SpawnObject():
+	var MyObject = preload("res://Sprites/fireball.jpg").instance()
+	$ObjectParent.add_child(MyObject)
+	MyObject.global_transform.origin = Vector3(-1000, 0, 0)
+	pass
